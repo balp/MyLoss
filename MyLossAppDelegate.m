7255fd6847574d58e459023dd0f212b232b5f98a
Anders Arnholm <balp@gwyneth.local>
2009-10-10 21:05:11 +0200

    Newly created application,

---
+++ MyLossAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  MyLossAppDelegate.m
+//  MyLoss
+//
+//  Created by Anders Arnholm on 2009-10-10.
+//  Copyright 2009 Mecel AB. All rights reserved.
+//
+
+#import "MyLossAppDelegate.h"
+
+@implementation MyLossAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MyLoss
+//
+//  Created by Anders Arnholm on 2009-10-10.
+//  Copyright 2009 Mecel AB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
