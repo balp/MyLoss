610e39795382ed4dcf50addcf1e932e5afc7773c
Anders Arnholm <Anders@Arnholm.se>
2009-10-11 14:58:20 +0200

    First Gui additions

---
+++ TodayPage.h
@@ -0,0 +1,14 @@
+//
+//  TodayPage.h
+//
+//  Created by Anders Arnholm on 2009-10-11.
+//  Copyright 2009 Mecel AB. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TodayPage : NSObject {
+
+}
+
+@end
---
+++ TodayPage.m
@@ -0,0 +1,12 @@
+//
+//  TodayPage.m
+//
+//  Created by Anders Arnholm on 2009-10-11.
+//  Copyright 2009 Mecel AB. All rights reserved.
+//
+
+#import "TodayPage.h"
+
+@implementation TodayPage
+
+@end
